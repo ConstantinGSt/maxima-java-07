3c989764db0ea007bf5f2326a87dcb9f1b9c9249
Constantin St <dinscalin.ks@gmail.com>
2022-11-23 10:04:01 +0700

    Pattern DAO 2.0

--- src/main/java/org/example/BaseRepository.java
+++ src/main/java/org/example/BaseRepository.java
@@ -2,10 +2,10 @@ package org.example;
 
 public interface BaseRepository<T> {
 
-    public void Create();
-    public void Read();
-    public void Update();
-    public void Delete();
-    public void findAll();
-    
+    public void Create(T... value);
+    public T Read(T... value);
+    public void Update(T... value);
+    public void Delete(T... value);
+    public T findAll();
+
 }
