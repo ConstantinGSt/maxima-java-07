a37158370b67fd1c4d0c48944f4c1015ba240672
ConstantinGSt <stepanyuk.kon@yandex.ru>
2022-12-04 18:14:49 +0700

    add

--- src/main/java/org/example/repository/CatRepository.java
+++ src/main/java/org/example/repository/CatRepository.java
@@ -2,7 +2,7 @@ package org.example.repository;
 
 import org.example.model.Cat;
 
-public interface CatRepository extends BaseRepository<Cat, Long> {
+public interface CatRepository extends BaseRepository<Cat, Integer> {
     // можно накиддачть че то что бы расширить
     // конкретный тип который уже у нас будет
     //GUID java.util.UUID.randomUUID();
--- src/test/java/org/example/CatRepositoryTest.java
+++ src/test/java/org/example/CatRepositoryTest.java
@@ -1,5 +1,6 @@
 package org.example;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 import org.example.model.Cat;
@@ -33,9 +34,29 @@ public class CatRepositoryTest {
         repository.create(cat5);
 
         List<Cat> cats = repository.findAll();
+        assertEquals(5, cats.size());
+
+        Cat testCat1 = repository.read(3);
+        assertEquals("Barsik", testCat1.getName());
+        assertEquals(0, testCat1.getWeight());
+        assertTrue(testCat1.isAngry());
 
         Cat newCat1 = new Cat(5,"Shipa", 15,false);
-        Cat newCat2 = new Cat(3,"Barsik", 10,false);
+        Cat newCat2 = new Cat(3,"BarsUk", 10,false);
+
+        repository.update(newCat1.getId(), newCat1);
+        repository.update(newCat2.getId(), newCat2);
+
+        repository.delete(1);
+        repository.delete(4);
+
+        cats = repository.findAll();
+
+        Cat testCat = repository.read(1);
+        Cat testCat2 = repository.read(2);
+        Cat testCat3 = repository.read(3);
+        Cat testCat4 = repository.read(4);
+        Cat testCat5 = repository.read(5);
 
         assertTrue(true);
     }
